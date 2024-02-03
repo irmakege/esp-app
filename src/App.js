24a87a9d6cdf8c3e861eb37d950ef89e3272054d
Irmak Ege <irmakege5@gmail.com>
2024-02-03 20:39:01 +0300

    private route added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Login from "./pages/Login"
-import Home from "./pages/Home"
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
 
 
 function App() {
+
+  const currentUser = false;
+
+  const PrivateRoute = ({children}) => {
+    return currentUser ? children : <Navigate to="/"/>;
+  }
+
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/home" element={<Home />} />
+        <Route path="/home" element={<PrivateRoute><Home /></PrivateRoute>} />
       </Routes>
     </Router>
 
